9636b1aff6718b16a1b8924cb5687be4fbb63815
Maud LEFORT <m.lefort@dydaps.com>
2026-08-13 22:18:05 +0200

    :lock: Add directory access guards and align composer tooling
    :shield: Added missing index.php guards to grid column and PrestaShop template directories
    :wrench: Added phpunit dev dependency and composer test and code style scripts
    :art: Tracked the PHP CS Fixer configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+$config = new PrestaShop\CodingStandards\CsFixer\Config();
+
+/** @var \Symfony\Component\Finder\Finder $finder */
+$finder = $config->setUsingCache(true)->getFinder();
+$finder
+    ->in(__DIR__)
+    ->exclude('vendor');
+
+return $config;
---
+++ src/Grid/Column/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 2007-2026 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ *
+ * @author    DYDAPS
+ * @copyright 2007-2026 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+header('HTTP/1.1 403 Forbidden');
+exit;
